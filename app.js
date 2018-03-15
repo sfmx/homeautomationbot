51acadc6a60ac6475ae7ce7eff141eb7e3ec969a
Jason Smith <smithj6@qut.edu.au>
2018-03-15 21:10:33 +1000

    created

---
+++ app.js
@@ -0,0 +1,68 @@
+/*-----------------------------------------------------------------------------
+A simple echo bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
+
+var restify = require('restify');
+var builder = require('botbuilder');
+var botbuilder_azure = require("botbuilder-azure");
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata 
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
+
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// Create your bot with a function to receive messages from the user
+var bot = new builder.UniversalBot(connector);
+bot.set('storage', tableStorage);
+
+// Make sure you add code to validate these fields
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+// Main dialog with LUIS
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+
+bot.dialog('TurnOnDialog',
+    (session, args) => {
+        // Resolve and store any HomeAutomation.Device entity passed from LUIS.
+        var intent = args.intent;
+        var device = builder.EntityRecognizer.findEntity(intent.entities, 'HomeAutomation.Device');
+
+        // Turn on a specific device if a device entity is detected by LUIS
+        if (device) {
+            session.send('Ok, turning on the %s.', device.entity);
+            // Put your code here for calling the IoT web service that turns on a device
+        } else {
+            // Assuming turning on lights is the default
+            session.send('Ok, turning on the lights');
+            // Put your code here for calling the IoT web service that turns on a device
+        }
+        session.endDialog();
+    }
+).triggerAction({
+    matches: 'HomeAutomation.TurnOn'
+})
